4764a81b8d8d9d302e32441e17d6b327e9bb0373
SillyBaka <1017165985@qq.com>
2023-04-01 19:50:31 +0800

    lab2D-v2.9: do not want to change again

--- src/raft/util.go
+++ src/raft/util.go
@@ -3,9 +3,9 @@ package raft
 import "log"
 
 // Debugging
-const Debug = true
+const Debug = false
 
-const Lock = true
+const Lock = false
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug {
